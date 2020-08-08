9f099cf3e619e8e49229990e51fafdd73b1f9eae
sziddhant <sziddhant@gmail.com>
2020-08-08 15:10:40 +0530

    Modified to run with videos in python3

--- demo.py
+++ demo.py
@@ -1,5 +1,3 @@
-# Author: Deepak Pathak (c) 2016
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,40 +7,47 @@ from PIL import Image
 import time
 import argparse
 import pyflow
+import cv2
+import pickle
 
-parser = argparse.ArgumentParser(
-    description='Demo for python wrapper of Coarse2Fine Optical Flow')
-parser.add_argument(
-    '-viz', dest='viz', action='store_true',
-    help='Visualize (i.e. save) output of flow.')
-args = parser.parse_args()
+cap = cv2.VideoCapture("Blender.avi")
+ret, im1 = cap.read()
 
-im1 = np.array(Image.open('examples/car1.jpg'))
-im2 = np.array(Image.open('examples/car2.jpg'))
-im1 = im1.astype(float) / 255.
-im2 = im2.astype(float) / 255.
+frames = []
+result = []
+frames.append(im1)
+while(1):
+    ret, im1 = cap.read()
+    if ret:
+        frames.append(im1)
+    else:
+        break
 
-# Flow Options:
-alpha = 0.012
-ratio = 0.75
-minWidth = 20
-nOuterFPIterations = 7
-nInnerFPIterations = 1
-nSORIterations = 30
-colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+for i in range(len(frames)-1):
+    im1 = frames[i]
+    im2 = frames[i+1]
+    im1 = im1.astype(float) / 255.
+    im2 = im2.astype(float) / 255.
 
-s = time.time()
-u, v, im2W = pyflow.coarse2fine_flow(
-    im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
-    nSORIterations, colType)
-e = time.time()
-print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
-    e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
-flow = np.concatenate((u[..., None], v[..., None]), axis=2)
-np.save('examples/outFlow.npy', flow)
+    # Flow Options:
+    alpha = 0.012
+    ratio = 0.75
+    minWidth = 20
+    nOuterFPIterations = 7
+    nInnerFPIterations = 1
+    nSORIterations = 30
+    colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
 
-if args.viz:
-    import cv2
+    s = time.time()
+    u, v, im2W = pyflow.coarse2fine_flow(
+        im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+        nSORIterations, colType)
+    e = time.time()
+    print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
+        e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
+    flow = np.concatenate((u[..., None], v[..., None]), axis=2)
+    # np.save('examples/outFlow.npy', flow)
+
     hsv = np.zeros(im1.shape, dtype=np.uint8)
     hsv[:, :, 0] = 255
     hsv[:, :, 1] = 255
@@ -50,5 +55,23 @@ if args.viz:
     hsv[..., 0] = ang * 180 / np.pi / 2
     hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    cv2.imwrite('examples/outFlow_new.png', rgb)
-    cv2.imwrite('examples/car2Warped_new.jpg', im2W[:, :, ::-1] * 255)
+    result.append(rgb)
+    print(i)
+
+    
+with open("Blender.txt", "wb") as fp:
+    pickle.dump(result, fp)
+
+with open("Blender.txt", "rb") as fp:
+    res = pickle.load(fp)
+
+for i in range(len(res)):
+    rgb = res[i]
+    cv2.imshow('frame2', rgb)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv2.imwrite('opticalfb.png',frame2)
+        cv2.imwrite('opticalhsv.png',rgb)
+
---
+++ test.py
@@ -0,0 +1,17 @@
+import cPickle as pickle
+import cv2
+
+with open("Blender.txt", "rb") as fp:
+    res = pickle.load(fp)
+
+for i in range(len(res)):
+    rgb = res[i]
+    cv2.imshow('frame2', rgb)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv2.imwrite('opticalfb.png',frame2)
+        cv2.imwrite('opticalhsv.png',rgb)
+    # break
+    # prvs = next
