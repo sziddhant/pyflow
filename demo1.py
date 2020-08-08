96f5f16ae2248efeb798a44493083a777b75abec
sziddhant <sziddhant@gmail.com>
2020-08-08 21:01:23 +0530

    Test

--- demo1.py
+++ demo1.py
@@ -9,69 +9,79 @@ import argparse
 import pyflow
 import cv2
 import pickle
+import multiprocessing
+from multiprocessing import Pool
 
-cap = cv2.VideoCapture("Blender.avi")
-ret, im1 = cap.read()
 
-frames = []
-result = []
-frames.append(im1)
-while(1):
-    ret, im1 = cap.read()
-    if ret:
-        frames.append(im1)
-    else:
-        break
-
-for i in range(len(frames)-1):
-    im1 = frames[i]
-    im2 = frames[i+1]
-    im1 = im1.astype(float) / 255.
-    im2 = im2.astype(float) / 255.
-
-    # Flow Options:
-    alpha = 0.012
-    ratio = 0.75
-    minWidth = 20
-    nOuterFPIterations = 7
-    nInnerFPIterations = 1
-    nSORIterations = 30
-    colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
-
+num_cores = multiprocessing.cpu_count()
+for ii in range(2):
     s = time.time()
-    u, v, im2W = pyflow.coarse2fine_flow(
-        im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
-        nSORIterations, colType)
+
+    cap = cv2.VideoCapture("Blender.avi")
+    ret, im1 = cap.read()
+
+    frames = []
+    frames.append(im1)
+    while(1):
+        ret, im1 = cap.read()
+        if ret:
+            #cv2.imshow('frame2', im1)
+            #cv2.waitKey(0)
+            frames.append(im1)
+        else:
+            break
+
+    #for i in range(len(frames)-1):
+    def func (i):
+        im1 = frames[i]
+        im2 = frames[i+1]
+        im1 = im1.astype(float) / 255.
+        im2 = im2.astype(float) / 255.
+
+        # Flow Options:
+        alpha = 0.012
+        ratio = 0.75
+        minWidth = 20
+        nOuterFPIterations = 7
+        nInnerFPIterations = 1
+        nSORIterations = 30
+        colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+
+        
+        u, v, im2W = pyflow.coarse2fine_flow(
+            im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+            nSORIterations, colType)
+        
+        #print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
+        #    e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
+        flow = np.concatenate((u[..., None], v[..., None]), axis=2)
+        # np.save('examples/outFlow.npy', flow)
+
+        hsv = np.zeros(im1.shape, dtype=np.uint8)
+        hsv[:, :, 0] = 255
+        hsv[:, :, 1] = 255
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang * 180 / np.pi / 2
+        hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+        rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        return rgb
+        print(i)
+
+    dataset=[i for i in range(0,29)]
+
+    agents = 30
+    chunksize = 1
+    with Pool(processes=agents) as pool:
+        result = pool.map(func, dataset, chunksize)
+
+    out = cv2.VideoWriter('O_out'+str(ii)+".avi",  
+                         cv2.VideoWriter_fourcc(*'MJPG'), 
+                         30, (640,480))   
+    for i in range(len(result)):
+        rgb = result[i]
+        out.write(rgb)
+    out.release()
+    cap.release()
     e = time.time()
-    print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
-        e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
-    flow = np.concatenate((u[..., None], v[..., None]), axis=2)
-    # np.save('examples/outFlow.npy', flow)
-
-    hsv = np.zeros(im1.shape, dtype=np.uint8)
-    hsv[:, :, 0] = 255
-    hsv[:, :, 1] = 255
-    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    result.append(rgb)
-    print(i)
-
+    print("%d in %.2fs"% (ii,e-s))
     
-with open("Blender.txt", "wb") as fp:
-    pickle.dump(result, fp)
-
-with open("Blender.txt", "rb") as fp:
-    res = pickle.load(fp)
-
-for i in range(len(res)):
-    rgb = res[i]
-    cv2.imshow('frame2', rgb)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    elif k == ord('s'):
-        cv2.imwrite('opticalfb.png',frame2)
-        cv2.imwrite('opticalhsv.png',rgb)
-
